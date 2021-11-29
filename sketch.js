7af196cdb5dea4d43c90453cf9021a3000706e18
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-29 16:22:52 -0300

    29-11

--- sketch.js
+++ sketch.js
@@ -1,51 +1,22 @@
-var mousevivo, mouse;
-var mouse1, mouse2, mouse3, mouse4;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(800,400);
-  mouse = createSprite(200, 200, 50, 80);
-  mouse.shapeColor = "green";
-  mouse.debug = true;
-  mousevivo = createSprite(400,800,80,30);
-  mousevivo.shapeColor = "green";
-  mousevivo.debug = true;
-  console.log(mouse.width/2);
-  console.log(mousevivo.width/2);
+var engine, world;
+var box1;
 
-  mouse1 = createSprite(100,100,50,50);
-  mouse1.shapeColor = "green";
-  mouse2 = createSprite(200,100,50,50);
-  mouse2.shapeColor = "green";
-  mouse3 = createSprite(300,100,50,50);
-  mouse3.shapeColor = "green";
-  mouse4 = createSprite(400,100,50,50);
-  mouse4.shapeColor = "green";
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    box1 = new Box(200,100,50,50);
+
 }
 
-function draw() {
-  background(255,255,255); 
-  mousevivo.x = World.mouseX;
-  mousevivo.y = World.mouseY; 
+function draw(){
+    background(0);
+    Engine.update(engine);
 
- if(isTouching(mouse3, mousevivo)){
-   mousevivo.shapeColor = "yellow";
-   mouse3.shapeColor = "yellow";
- }
- else{
-   mousevivo.shapeColor = "green";
-   mouse3.shapeColor = "green";
- }
-
-  drawSprites();
-}
-
-function isTouching(obj1, obj2){
-  if( obj1.x - obj2.x < obj2.width/2 + obj1.width/2                                                                                   
-    &&obj2.x - obj1.x < obj2.width/2 + obj1.width/2  
-    &&obj1.y - obj2.y < obj2.height/2 + obj1.height/2                                                                                   
-    &&obj2.y - obj1.y < obj2.height/2 + obj1.height/2  ){
-    return true;
-  } else {
-    return false;
-  }
+    box1.display();
+    
 }
