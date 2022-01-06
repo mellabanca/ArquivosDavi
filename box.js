25d767f4acd2c41d553ce819522b70680e1e88d7
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-01-06 16:16:35 -0300

    06/01/2022

--- box.js
+++ box.js
@@ -1,16 +1,27 @@
-class Box{
-    constructor(x,y,width,height){
-        var options = {
-            restitution: 1
-        }
-        this.body = Bodies.rectangle(x,y,width,height, options);
-
-        World.add(world,this.body);
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
     }
-    display(){
-        var pos = this.body.position;
-        rectMode(CENTER);
-        fill("white");
-        rect(pos.x,pos.y,this.width,this.height);
-    }
-}
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(4);
+    stroke("green");
+    fill(255);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
